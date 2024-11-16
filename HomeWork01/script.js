fcc95700c17d5edef5101451f31ac033b116c0e1
PolDog <manankov.pavel@gmail.com>
2024-11-16 23:45:15 +0300

    hw - 01

---
+++ HomeWork01/script.js
@@ -0,0 +1,46 @@
+console.log("Hw 01");
+const tempInCelcius=25;
+const tempInfarengheit=(9/5*tempInCelcius)+32;
+console.log("температура в фаренгейтах = "+tempInfarengheit);
+
+let name="test";
+let admin=name;
+console.log("значение переменной admin "+admin);
+/*
+let age=50;
+console.log(countClickButton);
+console.log(countClickButton);
+console.log(typeof(countClickButton));
+countClickButton=countClickButton+2;
+console.log(`count: ${countClickButton+1}`);
+
+
+let numberOne=5;
+let numberTwo="A";
+console.log(numberOne-numberTwo);
+
+
+console.log("объект");
+let student={
+    firstName:"Ivan",
+    lastname:"Petrov",
+    age:33,
+    faculty:"II"
+};
+console.log(`${student.firstName} ${student.lastname} ${student.age} yeards`);
+
+console.log("массив");
+
+let listOfBooks=['book "1" ','book "2" '];
+console.log(listOfBooks[1]);
+console.log(listOfBooks.length);
+
+console.log("функция");
+
+let x=function(y){
+    return y+2;
+}
+
+console.log(x.toString());
+console.log(x(10));
+*/
